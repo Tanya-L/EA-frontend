f6a40fc0a4f4a1ed4847ee291c458153fa4a5cfd
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-05-21 13:52:07 +0200

    refactor structure, add social sharing button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './layouts/home/home.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {OmOssComponent} from './components/om-oss/om-oss.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full' },
   {path: 'om-oss', component: OmOssComponent, pathMatch: 'full' },
   {path: 'contact', component: ContactComponent, pathMatch: 'full' },
+  {path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './layouts/home/home.component';
 import { NavBarComponent } from './layouts/nav-bar/nav-bar.component';
-import { FooterComponent } from './layouts/footer/footer.component';
-import { HeaderComponent } from './layouts/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { OmOssComponent } from './components/om-oss/om-oss.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { JwSocialButtonsModule } from 'jw-angular-social-buttons';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    FooterComponent,
-    HeaderComponent,
     ContactComponent,
-    OmOssComponent
+    OmOssComponent,
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    JwSocialButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
