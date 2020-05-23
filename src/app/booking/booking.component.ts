5e6f35669e947ec2bf578e13e4c04ccaccc842a9
Tanya Ly <tanya.lytovchenko@gmail.com>
2020-05-23 22:49:55 +0200

    add info about bookinh

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import * as Phone from '../components/contact/assets/headphones.png';
+
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
-  styleUrls: ['./booking.component.css']
-})
+  styleUrls: ['./booking.component.css', '../components/contact/contact.component.css']
+  })
 export class BookingComponent implements OnInit {
+  public phone: string = Phone.default;
 
   constructor() { }
 
